0b05b54fb2f267390651c77c9a7d779f9c4fea73
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-23 02:07:49 +0500

    new_todo_server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	todo "github.com/Hudayberdyyev/Rest_ToDo"
+	"log"
+)
 
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
