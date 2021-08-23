88853e4bd731c1b28090841d5078652fb90613ed
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-23 15:43:58 +0500

    format_code_and_add_bindings

--- cmd/main.go
+++ cmd/main.go
@@ -7,31 +7,32 @@ import (
 	"github.com/Hudayberdyyev/Rest_ToDo/pkg/service"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s\n", err.Error())
+		logrus.Fatalf("error initializing configs: %s\n", err.Error())
 	}
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s\n", err.Error())
+		logrus.Fatalf("error loading env variables: %s\n", err.Error())
 	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
-		Host: viper.GetString("db.host"),
-		Port: viper.GetString("db.port"),
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
-		DBName: viper.GetString("db.dbname"),
-		SSLMode: viper.GetString("db.sslmode"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
 		Password: os.Getenv("DB_PASSWORD"),
 	})
 
 	if err != nil {
-		log.Fatalf("failed to initialize db: %s\n", err.Error())
+		logrus.Fatalf("failed to initialize db: %s\n", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -40,7 +41,7 @@ func main() {
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
 
