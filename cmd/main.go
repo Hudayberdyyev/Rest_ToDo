0577064f4167a761fb25cce314274c12dff54de9
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-23 10:37:22 +0500

    add_handler_to_server

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	todo "github.com/Hudayberdyyev/Rest_ToDo"
+	"github.com/Hudayberdyyev/Rest_ToDo/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
