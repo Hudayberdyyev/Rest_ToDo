5387e8c9c4a33ee09b0566426a0d08ac7aef982e
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-23 11:51:41 +0500

    add_method_initConfig

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	todo "github.com/Hudayberdyyev/Rest_ToDo"
 	"github.com/Hudayberdyyev/Rest_ToDo/pkg/handler"
+	"github.com/Hudayberdyyev/Rest_ToDo/pkg/repository"
+	"github.com/Hudayberdyyev/Rest_ToDo/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s\n", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
