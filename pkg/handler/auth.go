d3ca7a6b99f24ea56ac1a82bd8adb89b431fa1cb
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-23 17:45:42 +0500

    error_response

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,8 +1,19 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	todo "github.com/Hudayberdyyev/Rest_ToDo"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
 
 }
 
