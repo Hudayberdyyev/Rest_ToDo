5f42a25ebacff1257593f798176a0acc7c0d4faa
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-24 15:09:33 +0500

    binding_for_user_model

--- user.go
+++ user.go
@@ -2,7 +2,7 @@ package todo
 
 type User struct {
 	Id       int    `json:"-"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"name"`
+	Username string `json:"username" binding:"username"`
+	Password string `json:"password" binding:"password"`
 }
