4cdbbf6769b362168d627addcb6ede85698b7353
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-24 15:59:20 +0500

    fix_bindings

--- user.go
+++ user.go
@@ -2,7 +2,7 @@ package todo
 
 type User struct {
 	Id       int    `json:"-"`
-	Name     string `json:"name" binding:"name"`
-	Username string `json:"username" binding:"username"`
-	Password string `json:"password" binding:"password"`
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
