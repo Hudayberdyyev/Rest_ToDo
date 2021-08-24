87a8e1fb4dfc16d4be863afff64befaef1f1b186
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-25 01:27:55 +0500

    service_todo_list_go

---
+++ pkg/service/todo_list.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	todo "github.com/Hudayberdyyev/Rest_ToDo"
+	"github.com/Hudayberdyyev/Rest_ToDo/pkg/repository"
+)
+
+type TodoListService struct {
+	repo repository.TodoList
+}
+
+func NewTodoListService(repo repository.TodoList) *TodoListService {
+	return &TodoListService{ repo: repo }
+}
+
+func (s *TodoListService) CreateList(userId int, list todo.TodoList) (int, error) {
+	return s.repo.CreateList(userId, list)
+}
+
+func (s *TodoListService) GetAllLists(userId int) ([]todo.TodoList, error) {
+	return s.repo.GetAllLists(userId)
+}
