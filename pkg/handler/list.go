baaba31327ae9d30ded0f8ef98e279276d4515f9
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-25 01:28:09 +0500

    handler_get_all_lists

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,20 +1,54 @@
 package handler
 
 import (
+	todo "github.com/Hudayberdyyev/Rest_ToDo"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	if userId, ok := c.Get(userCtx); ok {
-		c.JSON(http.StatusOK, map[string]interface{}{
-			"id": userId,
-		})
+	userId, err := getUserId(c)
+	if err != nil {
+		return
 	}
+
+	var input todo.TodoList
+	if err = c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.TodoList.CreateList(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+type getAllListsResponse struct {
+	Data []todo.TodoList `json:"data"`
 }
 
 func (h *Handler) getAllLists(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	var lists []todo.TodoList
+	lists, err = h.services.TodoList.GetAllLists(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
 }
 
 func (h *Handler) getListById(c *gin.Context) {
