1d9abd590f24e14cbfe00b353128c4fa9e07a9cd
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-24 17:37:16 +0500

    handler_create_list_method_test

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,9 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) createList(c *gin.Context) {
-
+	if userId, ok := c.Get(userCtx); ok {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"id": userId,
+		})
+	}
 }
 
 func (h *Handler) getAllLists(c *gin.Context) {
