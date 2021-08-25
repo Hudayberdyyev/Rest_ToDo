bebcd0a557c6db812470f2c6c086144bf0220e9f
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-25 10:57:34 +0500

    db_tags_for_todolist_model

--- todo.go
+++ todo.go
@@ -1,9 +1,9 @@
 package todo
 
 type TodoList struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title" binding:"required"`
-	Description string `json:"description" binding:"required"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" binding:"required" db:"title"`
+	Description string `json:"description" binding:"required" db:"description"`
 }
 
 type UsersList struct {
