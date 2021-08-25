987a70359c7f3093fe97a430c3eeb726746ec49d
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-25 17:09:44 +0500

    repo_add_delete_update_lists

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	todo "github.com/Hudayberdyyev/Rest_ToDo"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type TodoListPostgres struct {
@@ -54,4 +56,40 @@ func (r *TodoListPostgres) GetListById(userId, listId int) (todo.TodoList, error
 										where ul.user_id=$1 and ul.list_id = $2`,todoListsTable, usersListsTable)
 	err := r.db.Get(&list, query, userId, listId)
 	return list, err
+}
+
+func (r *TodoListPostgres) Delete(userId, listId int) error {
+	query := fmt.Sprintf("delete from %s tl using %s ul where tl.id = ul.list_id and ul.user_id=$1 and ul.list_id=$2",
+		todoListsTable, usersListsTable)
+	_, err := r.db.Exec(query, userId, listId)
+	return err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
+	argv := make([]interface{}, 0)
+	setValues :=  make([]string, 0)
+	argc := 1
+
+	if input.Title != nil {
+		argv = append(argv, *input.Title)
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argc))
+		argc++
+	}
+
+	if input.Description != nil {
+		argv = append(argv, *input.Description)
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argc))
+		argc++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+	query := fmt.Sprintf("update %s tl set %s from %s ul where tl.id = ul.list_id and ul.list_id=$%d and ul.user_id=$%d",
+		todoListsTable, setQuery, usersListsTable, argc, argc+1)
+	argv = append(argv, listId, userId)
+
+	logrus.Debugf("updateQuery: %s", query)
+	logrus.Debugf("args: %s", argv)
+
+	_, err := r.db.Exec(query, argv...)
+	return err
 }
